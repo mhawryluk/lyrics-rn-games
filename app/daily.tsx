c8b97c70b30079a73b92c16f1715d7f201ff175b
mhawryluk <marcinhawrylukxx@gmail.com>
2025-01-24 15:28:34 +0100

    Implement basic lyric of the day card component

--- app/daily.tsx
+++ app/daily.tsx
@@ -4,7 +4,7 @@ import { View, Text, Pressable } from "react-native";
 
 export default function Daily() {
   return (
-    <View>
+    <View className="flex-1">
       <View className="flex-row gap-4 items-center justify-between self-stretch py-4">
         <Link href="/" asChild className="p-2 items-center">
           <Pressable>
@@ -21,15 +21,40 @@ export default function Daily() {
           Lyric of the Day
         </Text>
 
-        <Pressable className="p-2 items-center">
+        <View className="p-2 items-center opacity-0">
           <Ionicons
             name="menu"
             className="text-white"
             color="#B99257"
             size={24}
           />
-        </Pressable>
+        </View>
+      </View>
+
+      <View className="flex-1 pt-20">
+        <LyricCard />
       </View>
     </View>
   );
 }
+
+function LyricCard() {
+  return (
+    <View className="bg-[#B99257] rounded-xl justify-center text-center items-center py-8 px-4 shadow">
+      <Text className="text-white font-bold font-mono text-xl leading-10">
+        {lyrics}
+      </Text>
+
+      <Text className="text-white font-bold font-mono text-md leading-10">
+        "my tears ricochet" (folklore, 2020)
+      </Text>
+    </View>
+  );
+}
+
+const lyrics = `
+We gather stones, 
+never knowing what they'll mean
+Some to throw, 
+some to make a diamond ring
+`;
