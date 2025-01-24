469f70a2a66e1ce0a3e2f56e885c83cafe39676f
mhawryluk <marcinhawrylukxx@gmail.com>
2025-01-24 11:27:23 +0100

    Add NativeWind

--- app/index.tsx
+++ app/index.tsx
@@ -3,16 +3,14 @@ import { Pressable, Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Link href="./cryptogram" asChild>
+    <View className="flex-1 justify-center items-center bg-[#FDF7E9]">
+      <Link
+        href="./cryptogram"
+        asChild
+        className="bg-[#A9C7A5] p-6 rounded-3xl"
+      >
         <Pressable>
-          <Text>Cryptogram</Text>
+          <Text className="text-3xl text-black font-bold">Cryptogram</Text>
         </Pressable>
       </Link>
     </View>
